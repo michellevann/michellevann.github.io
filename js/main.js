bb81d7fde765b2c923f08570e5e5a7eb281495d1
Michelle Vann <michelle@dougvann.com>
2018-12-17 23:58:46 -0500

    stuff I changed to make it work

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
-let i = true
-
-function toggle(){
-  
-  if (i){
-    document.getElementById("target").classList.remove("flip-it")
-    i = false
-  }
-  else{
-    document.getElementById("target").classList.add("flip-it")
-    document.getElementById("target").classList.remove("lock")
-    i = true
-  }
-}
-
-function check(){
-  console.log(document.getElementById("target").classList)
-}
+document.getElementById("sent").style.display = "none"
 
 function lock(){
   document.getElementById("target").classList.remove("flip-it")
   document.getElementById("target").classList.add("lock")
   i = false
+}
+function submitEmail(){
+  event.preventDefault();
+  document.getElementById("form").style.display = "none"
+  document.getElementById("sent").style.display = ""
+  setTimeout(unLock, 3000);
+}
+function unLock(){
+  document.getElementById("target").classList.add("flip-it")
+  document.getElementById("target").classList.remove("lock")
+  document.getElementById("form").style.display = ""
+  document.getElementById("sent").style.display = "none"
+    i = true
 }
